f7eca7a285efcd28bbfda4e5cd9bf30f9bd2c47a
tk0038 <nguyentrikhanh123hk@gmail.com>
2021-09-20 14:33:54 +0700

    udate source

--- web_banhang/app/Http/Controllers/Home/HomeController.php
+++ web_banhang/app/Http/Controllers/Home/HomeController.php
@@ -229,7 +229,7 @@ class HomeController extends Controller
             if (isset($cart[$id])) {
                 $cart[$id]['quantity'] = $cart[$id]['quantity'] + 1;
                 $product->discount = $product->discount - $cart[$id]['quantity'];
-                if($product->discount <= 0) {
+                if($product->discount < 0) {
                     return response()->json([
                         'code' => 300,
                         'message' => 'success'
@@ -254,7 +254,7 @@ class HomeController extends Controller
                     ];
                 }
                 $product->discount = $product->discount - 1;
-                if($product->discount <= 0) {
+                if($product->discount < 0) {
                     return response()->json([
                         'code' => 300,
                         'message' => 'success'
